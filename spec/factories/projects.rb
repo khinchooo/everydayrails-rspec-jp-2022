f77752b61de24d99e41361238949ad4bda18e747
khinchooo <khinchooo@mm.cybermissions.co.jp>
2022-06-21 12:29:32 +0900

    Factoryの重複のこと

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,5 +4,27 @@ FactoryBot.define do
     description { "A test project." }
     due_on { 1.week.from_now }
     association :owner
+  
+    factory :project_due_yesterday do
+      due_on { 1.day.ago }
+    end
+
+    factory :project_due_today do
+      due_on { Date.current.in_time_zone }
+    end
+  
+    factory :project_due_tomorrow do
+      due_on { 1.day.from_now }
+    end
+
+    trait :due_yesterday do
+      due_on { 1.day.ago }
+    end
+    trait :due_today do
+      due_on { Date.current.in_time_zone }
+    end
+    trait :due_tomorrow do
+      due_on { 1.day.from_now }
+    end
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -26,6 +26,26 @@ RSpec.describe Project, type: :model do
     expect(project.errors[:name]).to include("can't be blank")
   end
 
+  describe "late status" do
+    it "is late when the due date is past today" do
+      # project = FactoryBot.create(:project_due_yesterday)
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
+
+    it "is on time when the due date is today" do
+      # project = FactoryBot.create(:project_due_today)
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
+    end
+    
+    it "is on time when the due date is in the future" do
+      # project = FactoryBot.create(:project_due_tomorrow)
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
+
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
     @user.projects.create(
